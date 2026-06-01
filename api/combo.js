e7e4bb4c2a595b6465fc99337c01700d5a607801
westonhledik-byte <westonhledik@gmail.com>
2026-06-01 16:52:40 -0700

    Delete api/combo.js

--- api/combo.js
+++
@@ -1,6 +0,0 @@
-import tricks from '../data/tricks.json';
-
-export default function handler(req, res) {
-  const pick = () => tricks[Math.floor(Math.random() * tricks.length)];
-  res.status(200).json([pick(), pick(), pick()]);
-}
